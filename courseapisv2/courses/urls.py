5c7ba9a554b41c62d042aba57e8fa5ea099cc236
unknown <nquyenminhnhat0503@gmail.com>
2024-11-24 14:56:21 +0700

    week 3

---
+++ courseapisv2/courses/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+r = DefaultRouter()
+r.register('categories', views.CategoryViewSet, basename='cate')
+r.register('courses', views.CourseViewSet, basename='course')
+r.register('lessons', views.LessonViewSet, basename='lesson')
+
+urlpatterns = [
+    path('', include(r.urls))
+]
